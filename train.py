458b8ce995f969dcabcb975a46b6ed2a6fa79b5e
sidharthgurbani <55674995+sidharthgurbani@users.noreply.github.com>
2020-12-04 07:59:09 -0600

    change

--- train.py
+++ train.py
@@ -99,6 +99,8 @@ def train(**kwargs):
         trainer.reset_meters()
         for ii, (imgs, sizes, gt_bboxes_, gt_labels_, gt_difficults_) in tqdm(enumerate(test_dataloader)):
             sizes = [sizes[0][0].item(), sizes[1][0].item()]
+            print(sizes)
+            print(type(sizes))
             scale = imgs[0].shape[3]/sizes[1]
             scale = at.scalar(scale)
             imgs, gt_bboxes_, gt_labels_ = imgs.cuda().float(), gt_bboxes_.cuda(), gt_labels_.cuda()
