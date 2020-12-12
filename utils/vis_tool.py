01903fc65706795d2d85001e9a15bd63c3816c28
sidharthgurbani <55674995+sidharthgurbani@users.noreply.github.com>
2020-12-11 23:10:43 -0600

    change

--- utils/vis_tool.py
+++ utils/vis_tool.py
@@ -57,7 +57,6 @@ def vis_image(img, ax=None):
         ax = fig.add_subplot(1, 1, 1)
     # CHW -> HWC
     # img = img.transpose((1, 2, 0))
-    print(img.shape)
     img = img.transpose(1,2,0)
     ax.imshow(img.astype(np.uint8))
     return ax
